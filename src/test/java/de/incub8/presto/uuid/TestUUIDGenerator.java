a4ea70bfe8cfe4d1c2c03c80241e43cc68e2c2c7
Jan Doerrenhaus <jandoerrenhaus@incub8.de>
2015-11-20 13:01:53 +0100

    Switched unit tests to assertions. (#3)

--- src/test/java/de/incub8/presto/uuid/TestUUIDGenerator.java
+++ src/test/java/de/incub8/presto/uuid/TestUUIDGenerator.java
@@ -1,6 +1,8 @@
 package de.incub8.presto.uuid;
 
-import org.testng.TestException;
+import java.util.UUID;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestUuidGenerator
@@ -9,71 +11,35 @@ public class TestUuidGenerator
     public void type1()
     {
         String uuid = UuidFunctions.uuid_type1();
-        if (uuid == null)
-        {
-            throw new TestException("must not be null");
-        }
-        if (uuid.length() == 0)
-        {
-            throw new TestException("must not be empty");
-        }
-        if (!uuid.matches("[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-1[A-Fa-f0-9]{3}-[89aAbB][A-Fa-f0-9]{3}-[A-Fa-f0-9]{12}"))
-        {
-            throw new TestException("not a type 1 uuid");
-        }
+        Assert.assertNotNull(uuid);
+        Assert.assertNotEquals(uuid.length(), 0);
+        Assert.assertEquals(UUID.fromString(uuid).version(), 1);
     }
 
     @Test
     public void type3()
     {
         String uuid = UuidFunctions.uuid_type3("foo", "bar");
-        if (uuid == null)
-        {
-            throw new TestException("must not be null");
-        }
-        if (uuid.length() == 0)
-        {
-            throw new TestException("must not be empty");
-        }
-        if (!uuid.matches("[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-3[A-Fa-f0-9]{3}-[89aAbB][A-Fa-f0-9]{3}-[A-Fa-f0-9]{12}"))
-        {
-            throw new TestException("not a type 3 uuid");
-        }
+        Assert.assertNotNull(uuid);
+        Assert.assertNotEquals(uuid.length(), 0);
+        Assert.assertEquals(UUID.fromString(uuid).version(), 3);
     }
 
     @Test
     public void type4()
     {
         String uuid = UuidFunctions.uuid_type4();
-        if (uuid == null)
-        {
-            throw new TestException("must not be null");
-        }
-        if (uuid.length() == 0)
-        {
-            throw new TestException("must not be empty");
-        }
-        if (!uuid.matches("[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-4[A-Fa-f0-9]{3}-[89aAbB][A-Fa-f0-9]{3}-[A-Fa-f0-9]{12}"))
-        {
-            throw new TestException("not a type 4 uuid");
-        }
+        Assert.assertNotNull(uuid);
+        Assert.assertNotEquals(uuid.length(), 0);
+        Assert.assertEquals(UUID.fromString(uuid).version(), 4);
     }
 
     @Test
     public void type5()
     {
         String uuid = UuidFunctions.uuid_type5("foo", "bar");
-        if (uuid == null)
-        {
-            throw new TestException("must not be null");
-        }
-        if (uuid.length() == 0)
-        {
-            throw new TestException("must not be empty");
-        }
-        if (!uuid.matches("[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-5[A-Fa-f0-9]{3}-[89aAbB][A-Fa-f0-9]{3}-[A-Fa-f0-9]{12}"))
-        {
-            throw new TestException("not a type 5 uuid");
-        }
+        Assert.assertNotNull(uuid);
+        Assert.assertNotEquals(uuid.length(), 0);
+        Assert.assertEquals(UUID.fromString(uuid).version(), 5);
     }
 }
